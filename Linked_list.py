7285eab7d3688c9c5c504aac7bf0c19c2921b658
dnjs5397 <dnjs5397@naver.com>
2021-06-26 13:44:11 +0900

    [Algorithm_Linked list]

--- Linked_list.py
+++ Linked_list.py
@@ -1,25 +1,7 @@
 # 사용할 공간을 미리 할당해줘야 하는 list의 불편함을 해소하고자
 # Linked List 이용 (메모리 자동할당)
-
-
-# ----------------------- Node 클래스만 구현하고 선언과 연결은 직접
-
-class Node:  # 선언 1
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class Node: # 선언 2 == 선언 1
-    def __init__(self,data,next = None):
-        self.data = data
-        self.next = next
-
-node1 = Node(1)
-node2 = Node(2)
-node1.next = node2
-head = node1
-
-# ------------------------- Node 클래스 내에서 선언과 연결 모두
+# 장점 : 데이터 공간 미리 할당 안해도 됨
+# 단점 : 데이터 탐색에 시간이 오래걸림(비효율적) / 중간 데이터 삭제 시 추가 연결작업 필요 / 
 
 class Node:
     def __init__(self, data, next=None):
@@ -32,16 +14,59 @@ def add(data):
         node = node.next # 탈출하면 마지막 노드
     node.next = Node(data) # 마지막 노드 뒤에 새 노드 생성
 
+
 node1 = Node(1)
 head = node1
 for i in range(1,10): # for i in range (a,b) -> a부터 시작해서 b개 -> a부터 b-1까지
     add(i)
 
 
-### Linked list 데이터 출력 (검색) ###
+# -------------------------- Linked list 데이터 출력 (검색)
 
 node = head
 while node.next: # 마지막 노드 전까지 데이터 출력
     print(node.data)
     node = node.next
 print(node.data) # 마지막 노드도 출력
+
+
+# -------------------------- Linked list 노드들 사이에 데이터 삽입
+node3 = Node(1.5) # node3 을 노드 사이에 삽입할 것임
+
+node = head
+search = True
+while search:
+    if (node.data) == 1: # 1다음에 1.5 삽입
+        search = False
+    else:
+        node = node.next
+
+node_next = node.next # 찾은 노드의 다음 노드 주소 저장
+node.next = node3
+node3.next = node_next
+
+# ---------------------------- 위 모든 기능을 클래스 내에 구현한 Linked list 만들기
+
+class Node:
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+class NodeManagement:
+    def __init__(self, data):
+        self.head = Node(data) # 노드를 새로 생성할때 head를 한번에 지정
+    
+    def add(self,data):
+        if self.head == None: # head 값이 없다면 새로 추가한 값이 head
+            self.head = Node(data)
+        else:
+            node = self.head # head 값이 존재한다면 head에서 마지막 노드까지 간 후 추가해줘야함
+            while node.next:
+                node = node.next
+            node.next = Node(data)
+
+    def print_all(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
